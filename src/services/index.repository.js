5ac4b6bcb375bf1a531e7eb173eeb58b34d997f9
Ivan Di Gruttola <ivansandigruttola@gmail.com>
2024-04-06 21:08:58 -0300

    -

--- src/services/index.repository.js
+++ src/services/index.repository.js
@@ -1,4 +1,8 @@
-import { Carrito, Products, Ticket, Users } from '../dao/factory.js';
+const { default: CarritoMongo } = await import("../DAO/mongo/carrito.mongo.js");
+const { default: TicketMongo } = await import("../DAO/mongo/ticket.mongo.js");
+const { default: UserMongo } = await import("../DAO/mongo/user.mongo.js");
+const { default: ProductsMongo } = await import("../DAO/mongo/products.mongo.js");
+
 import CarritoRepository from './carrito.repository.js';
 import ProductsRepository from './products.repository.js';
 import TicketRepository from './ticket.repository.js';
@@ -7,7 +11,7 @@ import UserRepository from './users.repository.js'
 import Mail from "../modules/mail.module.js";
 const mailModule = new Mail();
 
-export const carritoService = new CarritoRepository(new Carrito());
-export const productsService = new ProductsRepository(new Products());
-export const ticketService = new TicketRepository(new Ticket(), mailModule)
-export const userService = new UserRepository(new Users(), mailModule)
+export const carritoService = new CarritoRepository(new CarritoMongo());
+export const productsService = new ProductsRepository(new ProductsMongo());
+export const ticketService = new TicketRepository(new TicketMongo(), mailModule)
+export const userService = new UserRepository(new UserMongo(), mailModule)
